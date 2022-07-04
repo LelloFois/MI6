fc60768a8b5042346fa67e16a4ea2d87b104fb85
Katerina Sevcikova <katerinasevcikova@hotmail.cz>
2022-07-04 11:36:32 +0200

    edits

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -4,6 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Image;
+use App\Models\Alias;
+use App\Models\Status;
+
 
 class Person extends Model
 {
