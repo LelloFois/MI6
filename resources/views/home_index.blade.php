cd3fca961998970560a24da38d8c1d877c45ac46
Antonello Fois <antonello.fois1381@gmail.com>
2022-07-04 14:24:19 +0200

    last changes

--- resources/views/home_index.blade.php
+++ resources/views/home_index.blade.php
@@ -1,34 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{mix('/css/app.css')}}">
-    <script src="../js/script.js" defer></script>
-</head>
-<body>
-    <div class="nav-panel">
-        <img class="seal" src="https://classes.codingbootcamp.cz/assets/classes/1404/mi6-seal.png" alt="MI6 Seal">
-        <a href="#">Home</a>
-        <a href="#">People of Interest</a>
-        <div class="nav-control">
-            <button><</button>
-        </div>
-    </div>
-    <main class="content">
+@extends('layouts.main')
 
+
+@section('content')
+
+    <main class="content">
+        <h1>Welcome to MI6</h1>
     </main>
 
-<script>
-    const button = document.querySelector('.nav-control button')
-    const navPanel = document.querySelector('.nav-panel')
-        
-    button.addEventListener('click', () => {
-        navPanel.classList.toggle('hidden')
-        button.textContent = button.textContent == '>' ? '<' : '>'
-        })
-    </script>
-</body>
-</html>
+
+@endsection
